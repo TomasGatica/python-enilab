b7842760d5c54e9aeb44aa545e4ae520671af82a
tomasQL <tomasgaticagomez@gmail.com>
2024-05-20 20:00:20 -0400

    xd

--- cliente-mongo.py
+++ cliente-mongo.py
@@ -2,7 +2,7 @@ from pymongo.mongo_client import MongoClient
 from pymongo.server_api import ServerApi
 
 
-uri = "mongodb+srv://tomasgaticagomez:Mongodibi@cluster0.jgxq2yw.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0"
+uri = ""
 client = MongoClient(uri, server_api=ServerApi('1'))
 
 try:
